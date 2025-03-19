6fa2acbc9001e46b7a5684b513c4b56934890f33
ChaChaEatsFruits <aadinarayanraghu@gmail.com>
2025-03-19 19:39:33 +0530

    fixed validation

--- javascript/script.js
+++ javascript/script.js
@@ -12,18 +12,14 @@ function registerUser() {
 }
 
 function goHome() {
-    alert("Going back to home");
+    
     window.location.href = "index.html";
 }
 function goLogin(){
-    alert("Going to Login page");
+    
     window.location.href="login.html";
 }
 function goRegister(){
-    alert("Going to SignUp page");
+    
     window.location.href="registration.html";
-}
-function checkLogin(){
-    const email=document.getElementById("email").value;
-    const password=document.getElementById("password").value;
 }
--- javascript/validate.js
+++ javascript/validate.js
@@ -1,16 +1,20 @@
 const form=document.getElementById('loginForm');
 const nameInput=document.getElementById('name');
-const email=document.getElementById('email');
-const phone=document.getElementById('phone');
-const password=document.getElementById('password');
-const confirmPassword=document.getElementById('confirm_password');
+const emailInput=document.getElementById('email');
+const phoneInput=document.getElementById('phone');
+const passwordInput=document.getElementById('password');
+const confirmPasswordInput=document.getElementById('confirm_password');
 
 form.addEventListener('submit',(e)=>{
-    //e.preventDefault();
     let errors=[];
+    e.preventDefault();
     if (nameInput){
         //we are in signup
-        errors=getSignupFormErrors(nameInput.value,email.value,phone.value,password.value,confirmPassword.value);
+        errors=getSignupFormErrors(nameInput.value,emailInput.value,phoneInput.value,passwordInput.value,confirmPasswordInput.value);
+        if (errors.length>0){
+            e.preventDefault();
+            alert(errors.join('\n'));
+        }
     }
     else{
         //login page
@@ -20,26 +24,18 @@ form.addEventListener('submit',(e)=>{
 
 function getSignupFormErrors(name,email,phone,password,confirmPassword){
     let errors=[];
-    
     if (password!==confirmPassword){
         errors.push('Passwords do not match');
-        password.parentElement.classList.add='incorrect';
-        confirmPassword.parentElement.classList.add='incorrect';
+        passwordInput.parentElement.classList.add('incorrect');
+        confirmPasswordInput.parentElement.classList.add('incorrect');
     }
     var atpos = email.indexOf("@");
     var dotpos = email.lastIndexOf(".");
 
     if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= email.length) {
         errors.push('Invalid email address');
-        email.parentElement.classList.add='incorrect';
+        emailInput.parentElement.classList.add('incorrect');
     }
-    if (!phone.checkValidity()) {
-        errors.push('Invalid phone number');
-        phone.parentElement.classList.add='incorrect';
-    }
-
-    if (errors.length>0){
-        e.preventDefault();
-        
-    }
+    return errors;
+    
 }
