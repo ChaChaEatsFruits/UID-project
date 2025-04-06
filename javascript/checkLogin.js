360b5a8883355b3480678838277a277e5f5c3469
ChaChaEatsFruits <aadinarayanraghu@gmail.com>
2025-04-06 20:14:53 +0530

    log in works

--- javascript/checkLogin.js
+++ javascript/checkLogin.js
@@ -7,3 +7,8 @@ else{
     document.querySelectorAll(".not-logged-in").forEach(item=>item.style.display="block");
     document.querySelectorAll(".logged-in").forEach(item=>item.style.display="none");
 }
+document.querySelector(".log-out").addEventListener('click',()=>{
+    localStorage.setItem('isLoggedIn',false);
+    localStorage.removeItem('currentUser');
+    window.location.href="home.html";
+})
---
+++ javascript/fetchUserDetails.js
@@ -0,0 +1,14 @@
+const currentUserEmail = JSON.parse(localStorage.getItem("currentUser"));
+const users = JSON.parse(localStorage.getItem("users"));
+
+const matchedUser = users.find(user => user.email === currentUserEmail);
+
+if (matchedUser) {
+    const name = document.querySelector(".name");
+    const email = document.querySelector(".email");
+    const phno = document.querySelector(".phno");
+
+    name.innerHTML = matchedUser.name;
+    email.innerHTML = matchedUser.email;
+    phno.innerHTML = matchedUser.phone;
+}
